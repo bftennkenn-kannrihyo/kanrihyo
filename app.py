e4339d6fdd4434058f8a972db5370ebae0678561
bftennkenn-kannrihyo <bf.tennkenn@gmail.com>
2025-11-10 20:23:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,16 @@ from medical_tab import render as render_medical
 from bio_tab import render as render_bio
 from calendar_tab import render as render_calendar
 
-
 st.set_page_config(page_title="管理表", layout="wide")
 st.title("🏥 管理表")
 
-
 _tab_med, _tab_bio, _tab_cal = st.tabs(["医療", "生体", "カレンダー"])
 
-
 with _tab_med:
-render_medical()
-
+    render_medical()
 
 with _tab_bio:
-render_bio()
-
+    render_bio()
 
 with _tab_cal:
-render_calendar()
+    render_calendar()
